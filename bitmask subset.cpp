86ac5119adf05ca61978e96f26cc0a94c034db80
Gabriel Andrade <gabrielasantana1602@gmail.com>
2025-05-12 22:46:22 -0300

    Create bitmask subset.cpp

---
+++ bitmask subset.cpp
@@ -0,0 +1,50 @@
+// Bitmask Subset Enumeration
+// Complexity: <O(2ⁿn),O(1)>
+// by: Gabeee
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define int long long
+#define pb push_back
+#define eb emplace_back
+#define is insert
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define f(i,b,e) for (int i = (b);i < (e);++i)
+#define rf(i,b,e) for (int i = (b);i >= (e);--i)
+#define fa(i,a) for (auto i : (a))
+#define sz(x) ((int)(x).size())
+#define fi first
+#define se second
+#define mkp make_pair
+
+#define debug(x) cerr << #x << " = " << (x) << endl;
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+bool all_test = 0;
+
+void solve() {
+	int n; cin >> n;
+	vector<int> arr(n,0); f (i,0,n) cin >> arr[i];
+	f (mask,0,(1 << n)) {
+		vector<int> sub;
+		f (j,0,n) {
+			if (mask & (1 << j)) sub.eb(arr[j]);
+		}
+	}
+}
+
+signed main() {
+	ios::sync_with_stdio(0);
+	cin.tie(nullptr);
+	int t = 1; if (all_test) cin >> t;
+	while (t--) solve();
+	return 0;
+}
